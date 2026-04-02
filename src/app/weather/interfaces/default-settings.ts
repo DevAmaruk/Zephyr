7646958638931cfcd21329e6330430e6794f1aba
Amaruk <j.guthauser@ik.me>
2026-04-02 22:27:51 +0200

    feat(weather): add weather service (#3)
    - Adds the weather service which is responsible for querying the Open-Meteo Forecast API.
    - Exposes a method called **getWeather** that takes as parameters the coordinates of a city and specific measure units (temperature, wind speed and precipitation).
    - The data is sent to a dumb component (weather-card) to display the weather information.

---
+++ src/app/weather/interfaces/default-settings.ts
@@ -0,0 +1,9 @@
+import { SettingsInterface } from './settings-interface';
+
+export const DEFAULT_SETTINGS: SettingsInterface = {
+  language: 'fr',
+  theme: 'system',
+  temperatureUnit: 'celsius',
+  windSpeedUnit: 'kmh',
+  precipitationUnit: 'mm',
+};
