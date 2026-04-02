cc166b0e8f3bb7e7f79da905f7498ab087c1d714
Amaruk <j.guthauser@ik.me>
2026-04-02 22:32:12 +0200

    Revert "feat(weather): add weather service (#3)" (#4)
    This reverts commit 7646958638931cfcd21329e6330430e6794f1aba.

--- src/app/weather/interfaces/default-settings.ts
+++
@@ -1,9 +0,0 @@
-import { SettingsInterface } from './settings-interface';
-
-export const DEFAULT_SETTINGS: SettingsInterface = {
-  language: 'fr',
-  theme: 'system',
-  temperatureUnit: 'celsius',
-  windSpeedUnit: 'kmh',
-  precipitationUnit: 'mm',
-};
--- src/app/weather/interfaces/settings-interface.ts
+++
@@ -1,7 +0,0 @@
-export interface SettingsInterface {
-  language: 'en' | 'fr';
-  theme: 'system' | 'light' | 'dark';
-  temperatureUnit: 'celsius' | 'fahrenheit';
-  windSpeedUnit: 'kmh' | 'mph' | 'ms' | 'kn';
-  precipitationUnit: 'mm' | 'inch';
-}
