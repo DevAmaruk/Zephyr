f5c0d69e03e54bc69c73de3a0a3cda9824c6db58
DevAmaruk <j.guthauser@ik.me>
2026-04-02 16:48:22 +0200

    feat(settings): add [key]:value properties for the settings

--- src/app/weather/interfaces/settings-interface.ts
+++ src/app/weather/interfaces/settings-interface.ts
@@ -1,1 +1,7 @@
-export interface SettingsInterface {}
+export interface SettingsInterface {
+  language: 'en' | 'fr';
+  theme: 'system' | 'light' | 'dark';
+  temperatureUnit: 'celsius' | 'fahrenheit';
+  windSpeedUnit: 'kmh' | 'mph' | 'ms' | 'kn';
+  precipitationUnit: 'mm' | 'inch';
+}
