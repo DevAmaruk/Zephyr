a205c57902a78243b0a6e210b55795dba0625828
DevAmaruk <j.guthauser@ik.me>
2026-04-02 16:42:19 +0200

    feat(geocoding): add properties for geocoding

--- src/app/weather/interfaces/geocoding-interface.ts
+++ src/app/weather/interfaces/geocoding-interface.ts
@@ -1,1 +1,13 @@
-export interface GeocodingInterface {}
+export interface GeocodingInterface {
+  results: GeocodingResult[];
+}
+
+export interface GeocodingResult {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  country_code: string;
+  country: string;
+  postcodes: string[];
+}
