57e31adecefad0e2e12fbf04492e205d3d7dff38
Amaruk <j.guthauser@ik.me>
2026-04-03 09:55:56 +0200

    feat(saved-cities): add saved cities page (#11)
    Adds the Saved Cities page responsible for displaying the list of cities the user saved.
    
    - Implements the selection of a city via the onSelectCity() and calls the onGetWeather() to navigate to the homepage passing the city's data as queryParams.
    - Implements the deletion of a city via the onDeleteCity().
    - Navigates back to the homepage via the backToHomepage().

---
+++ src/app/weather/containers/saved-cities/saved-cities.ts
@@ -0,0 +1,39 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { SavedCities } from '../../services/saved-cities';
+import { SavedCitiesInterface } from '../../interfaces/saved-cities-interface';
+
+@Component({
+  selector: 'app-saved-cities-page',
+  imports: [],
+  templateUrl: './saved-cities-page.html',
+  styleUrl: './saved-cities-page.scss',
+})
+export class SavedCitiesPage {
+  private readonly _router = inject(Router);
+  private readonly savedCities = inject(SavedCities);
+
+  protected selectedCity?: SavedCitiesInterface;
+
+  protected savedCitiesList: SavedCitiesInterface[] = this.savedCities.getAll();
+
+  public onSelectCity(city: SavedCitiesInterface) {
+    this.selectedCity = city;
+    this.onGetWeather();
+  }
+
+  public onGetWeather() {
+    if (this.selectedCity) {
+      this._router.navigate(['/home'], { queryParams: { ...this.selectedCity } });
+    }
+  }
+
+  public onDeleteCity(city: SavedCitiesInterface) {
+    this.savedCities.delete(city);
+    this.savedCitiesList = this.savedCities.getAll();
+  }
+
+  public backToHomepage() {
+    this._router.navigate(['/home']);
+  }
+}
