23e9394475009b0cc4e635aba11951fe6d84b5bc
DevAmaruk <j.guthauser@ik.me>
2026-04-03 09:08:00 +0200

    feat(weather-card): add weather card component

---
+++ src/app/weather/components/weather-card/weather-card.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SettingsInterface } from '../../interfaces/settings-interface';
+import { WeatherCodeConversionPipe } from '../../pipes/conversions/weather-code-conversion-pipe';
+import { TemperatureUnitConversionPipe } from '../../pipes/conversions/temperature-unit-conversion-pipe';
+
+@Component({
+  selector: 'app-weather-card',
+  imports: [TemperatureUnitConversionPipe, WeatherCodeConversionPipe],
+  templateUrl: './weather-card.html',
+  styleUrl: './weather-card.scss',
+})
+export class WeatherCard {
+  @Input() currentTemperature?: number;
+  @Input() feelsLikeTemperature?: number;
+  @Input() cityName?: string;
+  @Input() weatherCode: number = 0;
+
+  @Input() temperatureUnit: SettingsInterface['temperatureUnit'] = 'celsius';
+  @Input() currentLanguage: SettingsInterface['language'] = 'fr';
+
+  @Output() saveCityButton = new EventEmitter<void>();
+  @Output() cardTapped = new EventEmitter<void>();
+
+  public onCardTapped(): void {
+    this.cardTapped.emit();
+  }
+
+  public onSaveCity(): void {
+    this.saveCityButton.emit();
+  }
+}
