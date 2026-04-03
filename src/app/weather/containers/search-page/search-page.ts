d4bcb9c85d758911d98454002614ae7b7a352c0b
Amaruk <j.guthauser@ik.me>
2026-04-03 09:39:03 +0200

    feat(search-page): add search-page (#10)
    Adds the search page responsible for displaying city search results and returning the selected city to the homepage.
    
    - Implements debounced autocomplete via onInputChange() to query the geocoding API as the user types
    - Stores the selected GeocodingResult city properties when a user selects a result
    - Navigates back to the homepage via onGetWeather() passing the selected city data as queryParams

---
+++ src/app/weather/containers/search-page/search-page.ts
@@ -0,0 +1,62 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Geocoding } from '../../services/geocoding';
+import { Settings } from '../../services/settings';
+import { GeocodingInterface, GeocodingResult } from '../../interfaces/geocoding-interface';
+import { SavedCitiesInterface } from '../../interfaces/saved-cities-interface';
+import { SettingsInterface } from '../../interfaces/settings-interface';
+
+@Component({
+  selector: 'app-search-page',
+  imports: [AsyncPipe],
+  templateUrl: './search-page.html',
+  styleUrl: './search-page.scss',
+})
+export class SearchPage implements OnInit {
+  /*
+    This page is responsible for searching a location by its name and uses the autocomplete from Open-Meteo to display suggestions as the user types. 
+    It will inject the Geocoding Service to fetch the coordinates of the selected location.
+    It will pass the coordinates, the name and country properties from the Geocoding results to the Homepage component through the URL.
+  */
+
+  private readonly geocodingService = inject(Geocoding);
+  private readonly _router = inject(Router);
+  private readonly settingsService = inject(Settings);
+
+  private readonly searchQuery$ = new Subject<string>();
+  protected geocodingData$?: Observable<GeocodingInterface>;
+  protected selectedCity?: SavedCitiesInterface;
+
+  settings: SettingsInterface = this.settingsService.getSettings();
+
+  ngOnInit() {
+    this.geocodingData$ = this.searchQuery$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((cityName) =>
+        this.geocodingService.getCoordinates(cityName, this.settings.language),
+      ),
+    );
+  }
+
+  public onSelectCity(city: GeocodingResult) {
+    this.selectedCity = {
+      name: city.name,
+      country: city.country,
+      lat: city.latitude,
+      lon: city.longitude,
+    };
+  }
+
+  public onInputChange(query: string) {
+    this.searchQuery$.next(query);
+  }
+
+  public onGetWeather() {
+    if (this.selectedCity) {
+      this._router.navigate(['/home'], { queryParams: { ...this.selectedCity } });
+    }
+  }
+}
