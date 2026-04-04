c786c6ad95b16255bdb051b1315031cddcab3829
DevAmaruk <j.guthauser@ik.me>
2026-04-04 17:07:52 +0200

    refactor(saved-cities): save + delete done by id comparison

--- src/app/weather/services/saved-cities.ts
+++ src/app/weather/services/saved-cities.ts
@@ -12,7 +12,7 @@ export class SavedCities {
 
   public save(city: SavedCitiesInterface): void {
     const savedCities = this.getAll();
-    if (savedCities.some((c) => c.name === city.name && c.country === city.country)) {
+    if (savedCities.some((c) => c.id === city.id)) {
       return; // City already saved, do not add again
     }
 
@@ -22,9 +22,12 @@ export class SavedCities {
 
   public delete(city: SavedCitiesInterface): void {
     const savedCities = this.getAll();
-    const updatedCities = savedCities.filter(
-      (c) => c.name != city.name && c.country != city.country,
-    );
+    const updatedCities = savedCities.filter((c) => c.id != city.id);
     localStorage.setItem('savedCities', JSON.stringify(updatedCities));
+  }
+
+  public isCitySaved(city: SavedCitiesInterface): boolean {
+    const savedCities = this.getAll();
+    return savedCities.some((c) => c.id === city.id);
   }
 }
