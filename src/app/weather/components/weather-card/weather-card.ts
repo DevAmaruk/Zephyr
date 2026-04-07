4951f890b2489dc1282ab6a1bda1baa18a6c9877
DevAmaruk <j.guthauser@ik.me>
2026-04-07 15:30:31 +0200

    style(weather-card): add image optimisation with ngSrc

--- src/app/weather/components/weather-card/weather-card.ts
+++ src/app/weather/components/weather-card/weather-card.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SettingsInterface } from '../../interfaces/settings-interface';
 import { WeatherCodeConversionPipe } from '../../pipes/conversions/weather-code-conversion-pipe';
 import { TemperatureUnitConversionPipe } from '../../pipes/conversions/temperature-unit-conversion-pipe';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-weather-card',
-  imports: [TemperatureUnitConversionPipe, WeatherCodeConversionPipe],
+  imports: [TemperatureUnitConversionPipe, WeatherCodeConversionPipe, NgOptimizedImage],
   templateUrl: './weather-card.html',
   styleUrl: './weather-card.scss',
 })
