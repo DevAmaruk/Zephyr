47c205dd55007867276a7b5a9678d8cb920e61d1
Amaruk <j.guthauser@ik.me>
2026-04-13 09:50:47 +0200

    feat(weather-detail-card): add weather detail card + some icons (#44)
    - Adds new dumb component weather-details-card responsible for displaying the current weather data except the wind
    - Adds some new icons for the wind-details-card and weather-details-card

---
+++ src/app/weather/components/weather-details-card/weather-details-card.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeatherDetailsCard } from './weather-details-card';
+
+describe('WeatherDetailsCard', () => {
+  let component: WeatherDetailsCard;
+  let fixture: ComponentFixture<WeatherDetailsCard>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WeatherDetailsCard],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(WeatherDetailsCard);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
